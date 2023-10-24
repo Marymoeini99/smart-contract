1bed528c35fbb784ef0e53bd9bb0dd581de75cc5
Bahador <99763738+BahadorGh@users.noreply.github.com>
2023-10-24 17:54:20 +0330

    Update Enums.sol

--- draft/Enums.sol
+++ draft/Enums.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
 
+enum trusted_3pl {
+    None,
+    Manufactury,
+    Branch
+}
+
+enum OrderStatus {
+    None,
+    Proposed,
+    Accepted,
+    Pending,
+    Delivering,
+    Fulfilled
+}
